addfa8691631fe4037dd8cfb3d88f7e07da8a9f0
Trang Ho <carepjan@gmail.com>
2012-02-26 17:08:01 +0700

    initialize project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bookkeeper::Application.config.secret_token = 'a131ae2e2f77c0e2fde2c2d0342414168587129d97b09a355d79494c54af69d9576d8516a29d99800187823615787e6e54cbeb1d9c8cad16de1f2e1170387c9f'
